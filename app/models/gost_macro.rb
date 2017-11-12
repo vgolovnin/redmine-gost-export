a28cb9cebb983728df0e2ca94569a42245b9ca6b
Victor Golovnin <victor.golovnin@gmail.com>
2017-11-12 16:56:12 +0300

    fix project scope

--- app/models/gost_macro.rb
+++ app/models/gost_macro.rb
@@ -1,6 +1,6 @@
 class GostMacro < ActiveRecord::Base
   validates_presence_of :title
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, scope: :project
   validates_presence_of :text
   belongs_to :project
 end
